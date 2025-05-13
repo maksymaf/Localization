7ad2b3793f6bf94ca83b6bac7ee637d7ddb78166
maksymaf <forbidden344@gmail.com>
2025-05-13 21:55:50 +0300

    first commit. project created

---
+++ main.js
@@ -0,0 +1,186 @@
+const semanticCore = {
+    logo: {
+        ua: 'Лого',
+        en: 'Logo',
+        fr: 'Logo',
+        pl: 'Logo',
+        nl: 'Logo',
+        sa: 'شعار',
+        cn: '标志',
+        hu: 'Logó',
+        es: 'Logo',
+        pt: 'Logo',
+        lt: 'Logotipas',
+        jp: 'ロゴ',
+    },
+    safety:{
+        ua: 'Безпека',
+        en: 'Safety',
+        fr: 'Sécurité',
+        pl: 'Bezpieczeństwo',
+        nl: 'Beveiliging',
+        sa: 'الأمان',
+        cn: '安全',
+        hu: 'Biztonság',
+        es: 'Seguridad',
+        pt: 'Segurança',
+        lt: 'Saugumas',
+        jp: 'セキュリティ',
+    },
+    help:{
+        ua: 'Допомога',
+        en: 'Help',
+        fr: 'Assistance',
+        pl: 'Pomoc',
+        nl: 'Hulp',
+        sa: 'مساعدة',
+        cn: '帮助',
+        hu: 'Segítség',
+        es: 'Ayuda',
+        pt: 'Ajuda',
+        lt: 'Pagalba',
+        jp: 'ヘルプ',
+    },
+    home: {
+        ua: 'Головна',
+        en: 'Home',
+        fr: 'Accueil',
+        pl: 'Strona główna',
+        nl: 'Home',
+        sa: 'الصفحة الرئيسية',
+        cn: '首页',
+        hu: 'Főoldal',
+        es: 'Inicio',
+        pt: 'Início',
+        lt: 'Pagrindinis puslapis',
+        jp: 'ホーム',
+    },
+    about: {
+        ua: 'Про нас',
+        en: 'About',
+        fr: 'À propos',
+        pl: 'O nas',
+        nl: 'Over ons',
+        sa: 'معلومات عنا',
+        cn: '关于我们',
+        hu: 'Rólunk',
+        es: 'Sobre nosotros',
+        pt: 'Sobre nós',
+        lt: 'Apie mus',
+        jp: '私たちについて',
+    },
+    contacts: {
+        ua: 'Контакти',
+        en: 'Contacts',
+        fr: 'Contact',
+        pl: 'Kontakt',
+        nl: 'Contact',
+        sa: 'اتصل بنا',
+        cn: '联系我们',
+        hu: 'Kapcsolat',
+        es: 'Contacto',
+        pt: 'Contato',
+        lt: 'Kontaktai',
+        jp: 'お問い合わせ',
+    },
+    title:{
+        ua: 'Привіт!',
+        en: 'Hi!',
+        fr: 'Salut!',
+        pl: 'Cześć!',
+        nl: 'Hallo!',
+        sa: 'مرحباً!',
+        cn: '你好!',
+        hu: 'Helló!',
+        es: '¡Hola!',
+        pt: 'Olá!',
+        lt: 'Labas!',
+        jp: 'こんにちは!',
+    },
+    paragraph:{
+        ua: "Шукаєту когось, хто б правильно, коротко та якісно переклав ваш матеріал? Вам пощастило опинитись тут! Ми можемо перекласти все на понад 20 мов! Наші люди дуже освічені та розумні. <br>Наша команда складається з носіїв мови та лінгвістів-поліглотів, які забезпечать вам надійний та зрозумілий переклад. Наша сучасна команда забезпечить вам розвиток і просування будь-якого бізнесу або матеріалу.",
+        en: "Looking for someone who can translate your content accurately, concisely, and professionally? You’re in the right place! We offer translations into more than 20 languages. Our team is made up of highly educated native speakers and multilingual linguists who deliver clear and reliable translations. Whether you’re growing a business or sharing your content with the world, we’re here to help you reach a global audience with confidence.",
+        fr: "Vous cherchez une traduction précise et professionnelle ? Vous êtes au bon endroit ! Nous traduisons en plus de 20 langues. Nos traducteurs natifs et polyglottes vous garantissent des traductions claires et fiables. Notre équipe moderne vous aidera à développer et promouvoir votre activité ou contenu.",
+        pl: "Szukasz kogoś, kto dokładnie, krótko i profesjonalnie przetłumaczy Twój materiał? Trafiłeś we właściwe miejsce! Możemy przetłumaczyć wszystko na ponad 20 języków! Nasi ludzie są wykształceni i inteligentni.Nasza drużyna składa się z native speakerów i poliglotów, którzy zapewnią Ci niezawodne i zrozumiałe tłumaczenie. Nasz nowoczesny zespół pomoże Ci w rozwoju i promocji każdego biznesu lub materiału.",
+        nl: "Zoek je iemand die je materiaal correct, kort en professioneel kan vertalen? Je hebt geluk, je bent hier! Wij kunnen alles vertalen naar meer dan 20 talen! Onze mensen zijn goed opgeleid en intelligent. <br>Ons team bestaat uit moedertaalsprekers en polyglotten die je een betrouwbare en duidelijke vertaling zullen bieden. Ons moderne team zal je helpen met de ontwikkeling en promotie van je bedrijf of materiaal.",
+        sa: "هل تبحث عن شخص يمكنه ترجمة موادك بشكل دقيق ومختصر ومحترف؟ لقد حالفك الحظ لأنك هنا! يمكننا ترجمة كل شيء إلى أكثر من 20 لغة! موظفونا ذوو تعليم عالٍ وذو ذكاء. <br> يتكون فريقنا من متحدثين أصليين ولغويين متعددين الذين سيضمنون لك ترجمة موثوقة وواضحة. سيساعدك فريقنا الحديث في تطوير وترويج أي عمل أو مادة.",
+        cn: "您正在寻找一个能正确、简洁且专业地翻译您的材料的人吗？您很幸运来到这里！我们可以将所有内容翻译成超过 20 种语言！我们的团队非常有学问和聪明。<br>我们的团队由母语者和多语言专家组成，他们将为您提供可靠且易于理解的翻译。我们现代化的团队将帮助您发展和推广任何业务或材料。",
+        hu: "Keres valakit, aki pontosan, tömören és professzionálisan lefordítja a tartalmát? Szerencséje van, hogy itt van! Több mint 20 nyelvre lefordíthatjuk a szövegeket! Szakértőink magasan képzettek és intelligensek. <br>A csapatunk anyanyelvi beszélőkből és többnyelvű nyelvészekből áll, akik biztosítják a megbízható és érthető fordítást. Modern csapatunk segít a vállalkozása vagy anyaga fejlesztésében és népszerűsítésében.",
+        es: "¿Buscas a alguien que traduzca tu material de manera precisa, concisa y profesional? ¡Estás de suerte, has llegado al lugar adecuado! Podemos traducir todo a más de 20 idiomas. Nuestro equipo está compuesto por personas altamente educadas e inteligentes. <br>Nuestro equipo está formado por hablantes nativos y lingüistas políglotas que te asegurarán una traducción confiable y comprensible. Nuestro equipo moderno te ayudará a desarrollar y promover cualquier negocio o material.",
+        pt: "Procurando alguém para traduzir seu material de forma precisa, concisa e profissional? Você teve sorte, está no lugar certo! Podemos traduzir tudo para mais de 20 idiomas! Nossas pessoas são altamente educadas e inteligentes. <br> Nossa equipe é composta por falantes nativos e linguistas poliglotas que garantirão uma tradução confiável e compreensível. Nossa equipe moderna ajudará no desenvolvimento e promoção de qualquer negócio ou material.",
+        lt: 'Ieškote žmogaus, kuris tiksliai, glaustai ir kokybiškai išverstų jūsų medžiagą? Jums pasisekė – atsidūrėte tinkamoje vietoje! Mes galime išversti viską į daugiau nei 20 kalbų! Mūsų žmonės yra labai išsilavinę ir protingi. <br>Mūsų komanda susideda iš gimtakalbių ir daugiakalbių kalbininkų, kurie užtikrins patikimą ir aiškų vertimą. Mūsų šiuolaikinė komanda padės plėtoti ir reklamuoti bet kokį verslą ar medžiagą.',
+        jp: 'あなたの資料を正確・簡潔・高品質に翻訳してくれる人をお探しですか？ラッキーですね！ここにいます！私たちは20以上の言語に翻訳できます。私たちのスタッフはとても教育を受けていて賢いです。<br>チームはネイティブスピーカーと言語に堪能な専門家で構成されており、信頼性が高く分かりやすい翻訳を提供します。現代的な私たちのチームは、どんなビジネスや資料の成長とプロモーションにも貢献します。'
+    }
+};
+
+const allLanguages = document.querySelectorAll('.lang-img');
+    logo = document.querySelector('.language-logo');
+    title = document.querySelector('.language-title');
+    paragraph = document.querySelector('.language-paragraph');
+    themmeToggle = document.querySelector('.themme-toggle');
+    links = document.querySelectorAll('a');
+    main = document.querySelector('.main');
+    header = document.querySelector('.header');
+let allLang = ['ua', 'en', 'fr', 'pl', 'jp', 'nl', 'sa', 'lt', 'pt', 'cn', 'hu', 'es'];
+let lang = 'en';
+
+
+allLanguages.forEach((language) => {
+    language.addEventListener('click', (e) => {
+        lang = language.alt;
+        changeURL();
+    });
+});
+
+function changeURL(){
+    location.href = window.location.pathname + '#' + lang;
+    changeLang();
+}
+
+function changeLang(){
+    let hash = (window.location.hash).substring(1);
+
+    if(!allLang.includes(hash)){
+        lang = 'en';
+        changeURL();
+    }else{
+        lang = hash;
+    }
+
+    for(let key in semanticCore){
+        document.querySelector('.language-' + key).innerHTML = semanticCore[key][lang];
+    }
+}
+
+changeLang();
+
+function changeColor(color, bgColor) {
+    header.style.backgroundColor = bgColor;
+    main.style.backgroundColor = bgColor;
+    logo.style.color = color;
+    title.style.color = color;
+    paragraph.style.color = color;
+    links.forEach(link => link.style.color = color);
+}
+
+function applyTheme(isDark) {
+    if (isDark) {
+        themmeToggle.classList.add('active');
+        changeColor('#fff', '#000');
+        localStorage.setItem('isDark', 'true');
+    } else {
+        themmeToggle.classList.remove('active');
+        changeColor('#000', '#f4eae0');
+        localStorage.setItem('isDark', 'false');
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    const isDark = localStorage.getItem('isDark') === 'true';
+    applyTheme(isDark);
+});
+
+themmeToggle.addEventListener('click', () => {
+    const isDark = localStorage.getItem('isDark') === 'true';
+    applyTheme(!isDark);
+});
